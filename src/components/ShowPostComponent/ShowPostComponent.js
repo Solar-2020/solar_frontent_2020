73daf086356941df20348ce61e088cafffb39b94
Nell <ADshishova@mail.ru>
2020-10-14 17:01:59 +0300

    fixed empty arrays

--- src/components/ShowPostComponent/ShowPostComponent.js
+++ src/components/ShowPostComponent/ShowPostComponent.js
@@ -18,14 +18,22 @@ function ShowPostComponent({data}) {
                 <div className="show-post-component__white-part__avatar-text__avatar"></div>
                 <div className="show-post-component__white-part__avatar-text__text">
                     <div className="show-post-component__white-part__avatar-text__text__name">Автор поста</div>
-                    <div className="show-post-component__white-part__avatar-text__text__data">30 сен. 2020</div>
+                    <div className="show-post-component__white-part__avatar-text__text__data">{dataComp.publishDate.split('T')[0]}</div>
                 </div>
                 <button className="show-post-component__white-part__avatar-text__star-button"/>
             </div>
-            <div className="show-post-component__white-part__post-text">{dataComp.text}</div>
-            <ShowInterviewComponent interview={dataComp.interviews[0]}/>
-            <ShowPhotosComponent photos={dataComp.photos} backendAddress={BACKEND_ADDRESS}/>
-            <ShowDocsComponent docs={dataComp.files} backendAddress={BACKEND_ADDRESS}/>
+            {dataComp.text && (
+                <div className="show-post-component__white-part__post-text">{dataComp.text}</div>
+            )}
+            {dataComp.interviews[0].text && (
+                <ShowInterviewComponent interview={dataComp.interviews[0]}/>
+            )}
+            {dataComp.photos.length > 0 && (
+                <ShowPhotosComponent photos={dataComp.photos} backendAddress={BACKEND_ADDRESS}/>
+            )}
+            {dataComp.files.length > 0 && (
+                <ShowDocsComponent docs={dataComp.files} backendAddress={BACKEND_ADDRESS}/>
+            )}
         </div>
     );
 }
