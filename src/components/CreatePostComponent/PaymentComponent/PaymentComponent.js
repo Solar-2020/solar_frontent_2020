7b1e189707cc26bed3152dc7448c5d0791deaad6
Andrey Kochnov <andreykochnov@yandex.ru>
2020-10-14 18:12:31 +0300

    Set payment summ to positive integers only

--- src/components/CreatePostComponent/PaymentComponent/PaymentComponent.js
+++ src/components/CreatePostComponent/PaymentComponent/PaymentComponent.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import './PaymentComp.css';
+import { IntegerInput, } from "react-native";
 
 /**
  * Component with payment
  * @param {object} param0 - handler for removing component
  * @return {jsx}
  */
-function PaymentComponent({delPaymentComp, changePaymentHandler}) {
+function PaymentComponent({ delPaymentComp, changePaymentHandler }) {
     return (
         <div className="payment-component">
             <div className="payment-component__title-close">
                 <div className="payment-component__title-close__title">Параметры оплаты</div>
-                <button className="payment-component__title-close__close-btn" onClick={delPaymentComp}/>
+                <button className="payment-component__title-close__close-btn" onClick={delPaymentComp} />
             </div>
             <div className="payment-component__payment-form">
-                <input
-                    placeholder="Сумма" 
-                    className="payment-component__payment-form__summ" 
+                <IntegerInput
+                    placeholder="Сумма"
+                    className="payment-component__payment-form__summ"
                     type="number"
-                    onChange={(e) => changePaymentHandler('cost', e.target.value)}/>
-                <select 
+                    step="1"
+                    pattern="\d+"
+                    onChange={(e) => changePaymentHandler('cost', e.target.value)} />
+                <select
                     className="payment-component__payment-form__list"
                     onChange={(e) => changePaymentHandler('currency', e.target.value)}>
                     <option value="1">Рубли</option>
-                    <option value="2">Доллары</option> 
+                    <option value="2">Доллары</option>
                 </select>
             </div>
         </div>
