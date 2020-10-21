fa5a47d216bbf246b0272f39e676a366fe4c6da5
Nell <ADshishova@mail.ru>
2020-10-21 14:41:36 +0300

    fixed month

--- src/utils/time.js
+++ src/utils/time.js
@@ -4,7 +4,7 @@
  */
 export function getNowTime() {
     const data = new Date();
-    let str = `${data.getFullYear()}-${data.getMonth()}-${data.getDate()}T${data.toTimeString(data.getTime).split(' ')[0]}.${data.getMilliseconds()}${data.toTimeString(data.getTime).split('GMT')[1].substr(0, 3)}:00`;
+    let str = `${data.getFullYear()}-${data.getMonth() + 1}-${data.getDate()}T${data.toTimeString(data.getTime).split(' ')[0]}.${data.getMilliseconds()}${data.toTimeString(data.getTime).split('GMT')[1].substr(0, 3)}:00`;
     str = str.replaceAll(':', '%3A');
     str = str.replaceAll('+', '%2B');
     return str;
