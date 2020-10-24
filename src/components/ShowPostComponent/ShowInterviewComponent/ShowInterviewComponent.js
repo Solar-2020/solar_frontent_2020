d6fffa565007b03564fc6097163d3cc5bcfc6b6a
Nell <ADshishova@mail.ru>
2020-10-24 18:55:15 +0300

    added onclickview

--- src/components/ShowPostComponent/ShowInterviewComponent/ShowInterviewComponent.js
+++ src/components/ShowPostComponent/ShowInterviewComponent/ShowInterviewComponent.js
@@ -10,6 +10,9 @@ import {BACKEND_ADDRESS} from '../../../utils/Config/Config.js';
  * @return {jsx}
  */
 function ShowInterviewComponent({interview, postId}) {
+    // переписать
+    const [view, setView] = useState(false);
+
     const [selectedItem, setSelectedItem] = useState([]);
     const [showAnswersRes, setShowAnswersRes] = useState({});
 
@@ -50,6 +53,9 @@ function ShowInterviewComponent({interview, postId}) {
                console.log(responseBody);
                if (responseBody.answers) {
                     createAnswersObject(responseBody.answers);
+
+                    // переписать
+                    setView(true);
                }
             });
     };
@@ -80,30 +86,46 @@ function ShowInterviewComponent({interview, postId}) {
                 <div className="show-post-component__white-part__show-interview-container__title">{interview.text}</div>
             )}
             {interview.status === 0 ? (
-                <form onSubmit={submitHandler}>
-                    {interview.type === 1 ? (
-                        <div>
-                            {interview.answers.map((answer) => (
-                            <div key={answer.id} className="show-post-component__white-part__show-interview-container__answer">
-                                <input type="radio" value={answer.id} name="answer"/>
-                                <div>{answer.text} </div>
+                <div>
+                    {!view && (
+                        <form onSubmit={submitHandler}>
+                            {interview.type === 1 ? (
+                                <div>
+                                    {interview.answers.map((answer) => (
+                                    <div key={answer.id} className="show-post-component__white-part__show-interview-container__answer">
+                                        <input type="radio" value={answer.id} name="answer"/>
+                                        <div>{answer.text} </div>
+                                    </div>
+                                ))}
+                                </div>
+                            ) : (
+                                <div>
+                                    {interview.answers.map((answer) => (
+                                        <div key={answer.id} className="show-post-component__white-part__show-interview-container__answer">
+                                            <input type="checkbox" value={answer.id} name="answer"/>
+                                            <div>{answer.text} </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            <div className="show-post-component__white-part__show-interview-container__form-button-container">
+                                <input className="show-post-component__white-part__show-interview-container__form-button-container__button" type="submit" value="Отправить ответ"/>
                             </div>
-                        ))}
-                        </div>
-                    ) : (
+                        </form>
+                    )}
+                    {view && (
                         <div>
                             {interview.answers.map((answer) => (
-                                <div key={answer.id} className="show-post-component__white-part__show-interview-container__answer">
-                                    <input type="checkbox" value={answer.id} name="answer"/>
-                                    <div>{answer.text} </div>
+                                <div key={answer.id}
+                                    className="show-post-component__white-part__show-interview-container__select-answer-container"
+                                    style={createStyle(showAnswersRes[String(answer.id)])}>
+                                    <div className="show-post-component__white-part__show-interview-container__select-answer">{answer.text}</div>
+                                    <div>{`${showAnswersRes[String(answer.id)]}%`}</div>
                                 </div>
                             ))}
                         </div>
                     )}
-                <div className="show-post-component__white-part__show-interview-container__form-button-container">
-                        <input className="show-post-component__white-part__show-interview-container__form-button-container__button" type="submit" value="Отправить ответ"/>
-                    </div>
-                </form>
+                </div>
             ) : (
                 <div>
                     {createAnswersObject(interview.answers) && (
