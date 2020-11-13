99207bf63a140cbc2fee88c70a90ad731b1af407
Nell <ADshishova@mail.ru>
2020-11-14 01:18:09 +0300

    added before replace

--- src/utils/time.js
+++ src/utils/time.js
@@ -11,6 +11,8 @@ export function getNowTime() {
     day = (day.length === 1) ? '0' + day : day;
 
     let str = `${data.getFullYear()}-${month}-${day}T${data.toTimeString(data.getTime).split(' ')[0]}.${data.getMilliseconds()}${data.toTimeString(data.getTime).split('GMT')[1].substr(0, 3)}:00`;
+    console.log('-----------');
+    console.log(str);
     str = str.replaceAll(':', '%3A');
     str = str.replaceAll('+', '%2B');
     return str;
