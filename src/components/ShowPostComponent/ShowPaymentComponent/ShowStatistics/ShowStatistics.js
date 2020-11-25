f116462c85a463c8598362ee764e992e21ad3459
Nell <ADshishova@mail.ru>
2020-11-25 14:55:37 +0300

    added statistic

--- src/components/ShowPostComponent/ShowPaymentComponent/ShowStatistics/ShowStatistics.js
+++ src/components/ShowPostComponent/ShowPaymentComponent/ShowStatistics/ShowStatistics.js
@@ -12,14 +12,17 @@ import {BACKEND_ADDRESS, CARD_BANK} from '../../../../utils/Config/Config';
  * @return {jsx}
  */
 function ShowStatistics({cookies, closeStat, paymentID}) {
-    
+    const [statistic, setStatistic] = useState([]);
     useEffect(
         () => {
             getStat();
         }, [paymentID]);
 
     function getStat() {
-        console.log('stat');
+        // console.log('stat');
+        // const responseBody = [{"payer":{"id":281,"email":"Ad.shishova@yandex.ru","name":"Анастасия","surname":"Шишова","avatarURL":"61207/enc-bccac8eccfa2ed0b4918b8f64687f983f23d9a96d2eccac2a697ce7cd339719d"},"paidID":4,"paymentID":69,"message":"","requisiteType":3,"paidAt":"2020-11-25T13:35:00.349584+03:00","cost":"11","requisite":{"youMoneyAccount":{"id":17,"accountNumber":"1111111111111111","owner":281}}},{"payer":{"id":281,"email":"Ad.shishova@yandex.ru","name":"Анастасия","surname":"Шишова","avatarURL":"61207/enc-bccac8eccfa2ed0b4918b8f64687f983f23d9a96d2eccac2a697ce7cd339719d"},"paidID":5,"paymentID":69,"message":"","requisiteType":3,"paidAt":"2020-11-25T14:22:07.739816+03:00","cost":"100","requisite":{"youMoneyAccount":{"id":17,"accountNumber":"1111111111111111","owner":281}}},{"payer":{"id":281,"email":"Ad.shishova@yandex.ru","name":"Анастасия","surname":"Шишова","avatarURL":"61207/enc-bccac8eccfa2ed0b4918b8f64687f983f23d9a96d2eccac2a697ce7cd339719d"},"paidID":6,"paymentID":69,"message":"100","requisiteType":3,"paidAt":"2020-11-25T14:22:28.843867+03:00","cost":"1","requisite":{"youMoneyAccount":{"id":17,"accountNumber":"1111111111111111","owner":281}}}];
+        // setStatistic(responseBody);
+
         fetchModule.get({
             url: BACKEND_ADDRESS + `/api/payment/stat/${paymentID}`,
             body: null,
@@ -35,7 +38,9 @@ function ShowStatistics({cookies, closeStat, paymentID}) {
                 return response.json();
             })
             .then((responseBody) => {
-                console.log(responseBody);
+                if (responseBody.payer) {
+                    setStatistic(responseBody);
+                }
             });
     };
 
@@ -45,6 +50,26 @@ function ShowStatistics({cookies, closeStat, paymentID}) {
                 <div className="show-stat__lightbox-container__title">
                     <div className="show-stat__lightbox-container__title__text">Статистика</div>
                     <button className="show-stat__lightbox-container__title__close-btn" onClick={() => closeStat()}/>
+                </div>
+                <div className="show-stat-table_margin">
+                    <table className="show-stat-table">
+                        <tbody>
+                            <tr>
+                                <th>Платильщик</th>
+                                <th>Почта</th>
+                                <th>Сумма</th>
+                                <th>Дата</th>
+                            </tr>
+                            {statistic.map((elem, index) => (
+                                <tr key={index}>
+                                    <td>{`${elem.payer.name} ${elem.payer.surname}`}</td>
+                                    <td>{elem.payer.email}</td>
+                                    <td>{elem.cost}</td>
+                                    <td>{elem.paidAt.split('T')[0]}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
